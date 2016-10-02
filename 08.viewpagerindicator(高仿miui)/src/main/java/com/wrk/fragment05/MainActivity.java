69c127f8c9e88b81b4ffc1cea69acbf2ddb88bb2
[Window Title] <1024057635@qq.com>
2016-10-03 00:34:23 +0800

    自定义ViewPager指示器---1---

---
+++ 08.viewpagerindicator(高仿miui)/src/main/java/com/wrk/fragment05/MainActivity.java
@@ -0,0 +1,88 @@
+package com.wrk.fragment05;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.wrk.fragment05.view.ViewPagerIndicator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+    private ViewPagerIndicator mIndicator;
+
+    private List<String> mTitles;
+
+    private List<Fragment> mContents;
+
+    private FragmentPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        initDatas();
+
+        mViewPager.setAdapter(mAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                // tabWidth*positionOffset + position*tabWidth
+                mIndicator.scroll(position, positionOffset);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void initDatas() {
+
+        mTitles = Arrays.asList("短信1", "收藏2", "推荐3","短信4", "收藏5", "推荐6","短信7", "收藏8", "推荐9");
+        mContents = new ArrayList<Fragment>();
+
+        for (String title : mTitles) {
+            VpSimpleFragment vpSimpleFragment = VpSimpleFragment.newInstance(title);
+            mContents.add(vpSimpleFragment);
+        }
+
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mContents.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mContents.size();
+            }
+        };
+
+    }
+
+    private void initViews() {
+
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        mIndicator = (ViewPagerIndicator) findViewById(R.id.id_indicator);
+
+    }
+}
---
+++ 08.viewpagerindicator(高仿miui)/src/main/java/com/wrk/fragment05/view/ViewPagerIndicator.java
@@ -0,0 +1,210 @@
+package com.wrk.fragment05.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.wrk.fragment05.R;
+
+/**
+ * Created by MrbigW on 2016/10/2.
+ * weChat:1024057635
+ * Github:MrbigW
+ * Usage: -.-
+ * -------------------=.=------------------------
+ */
+
+public class ViewPagerIndicator extends LinearLayout {
+
+    private Paint mPaint;
+
+    private Path mPath;
+
+    // 三角形的宽高
+    private int mTriangleWidth;
+    private int mTriangleHeight;
+
+    private static final float RADIO_TRIANGLE_WIDTH = 1 / 6F;
+
+    // 初始的偏移量
+    private int mInitTranslationX;
+
+    private int mTranslationX = 0;
+
+    private int mTabVisibleCount;
+
+    private static final int COUNT_DEFAULT_TAB = 4;
+
+
+    public ViewPagerIndicator(Context context) {
+        this(context, null);
+    }
+
+    public ViewPagerIndicator(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ViewPagerIndicator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        // 获取自定义属性(可见tab的数量)
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ViewPagerIndicator);
+        mTabVisibleCount = array.getInt(R.styleable.ViewPagerIndicator_visible_tab_count, COUNT_DEFAULT_TAB);
+        if (mTabVisibleCount < 0) {
+            mTabVisibleCount = COUNT_DEFAULT_TAB;
+        }
+        array.recycle();
+
+        // 初始化画笔
+        initPaint();
+
+    }
+
+    private void initPaint() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setPathEffect(new CornerPathEffect(3));
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        int cCount = getChildCount();
+        if (cCount == 0) {
+            return;
+        }
+        for (int i = 0; i < cCount; i++) {
+            View view = getChildAt(i);
+            LinearLayout.LayoutParams lp = (LayoutParams) view.getLayoutParams();
+            lp.weight = 0;
+
+            lp.width = getScreenWidth() / mTabVisibleCount;
+            view.setLayoutParams(lp);
+        }
+
+    }
+
+    private int getScreenWidth() {
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+
+        Log.e("111", mInitTranslationX + "---" + mTranslationX + "---" +
+                mTriangleHeight + "---" + mTriangleWidth + "---" + getHeight());
+
+        canvas.save();
+
+        canvas.translate(mInitTranslationX + mTranslationX, getHeight() + 4);
+
+        canvas.drawPath(mPath, mPaint);
+
+        canvas.restore();
+
+        super.dispatchDraw(canvas);
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mTriangleWidth = (int) (w / mTabVisibleCount * RADIO_TRIANGLE_WIDTH);
+
+        mInitTranslationX = w / mTabVisibleCount / 2 - mTriangleWidth / 2;
+
+        initTriangle();
+    }
+
+    /**
+     * 初始化三角形
+     */
+    private void initTriangle() {
+
+        mTriangleHeight = mTriangleWidth / 2;
+
+        mPath = new Path();
+        mPath.moveTo(0, 0);
+        mPath.lineTo(mTriangleWidth, 0);
+
+        mPath.lineTo(mTriangleWidth / 2, -mTriangleHeight);
+        mPath.close();
+
+    }
+
+    /**
+     * 指示器跟随手指进行滚动
+     *
+     * @param position
+     * @param positionOffset
+     */
+    public void scroll(int position, float positionOffset) {
+
+        int tabWidth = getWidth() / mTabVisibleCount;
+        mTranslationX = (int) (tabWidth * (positionOffset + position));
+
+        // 容器移动，当tab处于移动至最后一个时
+        if (position >= mTabVisibleCount - 2 && positionOffset > 0 && getChildCount() > mTabVisibleCount) {
+
+            if(mTabVisibleCount!= 1) {
+                this.scrollTo((int) ((position - (mTabVisibleCount - 2)) * tabWidth + tabWidth * positionOffset), 0);
+            }else {
+                this.scrollTo((int) (position * tabWidth + tabWidth*positionOffset),0);
+            }
+
+
+        }
+
+        invalidate();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
